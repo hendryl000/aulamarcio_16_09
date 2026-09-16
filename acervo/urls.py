6b54f38fd2826a66d18087a62b9fe50efd6b0a2f
hendryl000 <hendryl.alves@yahoo.com.br>
2026-09-16 20:36:01 -0300

    projeto inicial aula 4 e 5

---
+++ acervo/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("livros/", views.lista_livros, name="lista"),
+    path("livros/novo/", views.novo_livro, name="novo"),
+]
