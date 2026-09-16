c6470dc042ac05f8d619bd2748363b7c9cba52a3
hendryl000 <hendryl.alves@yahoo.com.br>
2026-09-16 20:52:34 -0300

    adicionando livro digital e fisico

--- acervo/models.py
+++ acervo/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 
 class Livro(models.Model):
+    
+    TIPO_ACERVO_CHOICES = [
+        ("DIGITAL", "Digital"),
+        ("FISICO", "Físico"),
+    ]
+
+    
     titulo = models.CharField(max_length=200)
     autor = models.CharField(max_length=100)
     ano = models.IntegerField()
